15e64cb3117564d1b523fdf39c0d6b169157c519
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2023-05-17 14:53:08 +0300

    Update roundageclickobject.js

--- roundageclickobject.js
+++ roundageclickobject.js
@@ -4,5 +4,5 @@ var objtoclick = loader.engine.document.getElementById(107356600);
 var roundedValue1 = Math.round(result1 * 1) / 1;
 loader.engine.document.getElementById(107353245).setValue(({"value": roundedValue1})); 
 };
-objtoclick.click()
+
 window.onchange = roundResult;
