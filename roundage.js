7c974c084005ba0e06fb7b4be1cbe67e2a3f40e9
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2023-05-17 14:56:45 +0300

    Update roundage.js

--- roundage.js
+++ roundage.js
@@ -1,4 +1,5 @@
 function roundResult() {
+var objtoclick = loader.engine.document.getElementById(107356600);
 var result1 = loader.engine.document.getElementById(107353245).getProperty('value.value'); 
 var roundedValue1 = Math.round(result1 * 1) / 1;
 loader.engine.document.getElementById(107353245).setValue(({"value": roundedValue1})); 
