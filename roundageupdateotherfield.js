097d6d602fb0efde10e4c62a4800c3bccede9624
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2023-05-17 19:36:03 +0300

    Update roundageupdateotherfield.js

--- roundageupdateotherfield.js
+++ roundageupdateotherfield.js
@@ -3,9 +3,9 @@ var result1 = loader.engine.document.getElementById(107353245).getProperty('valu
 var roundedValue1 = Math.round(result1 * 1) / 1;
 loader.engine.document.getElementById(107363803).setValue(({"value": roundedValue1})); 
 
-var result1 = loader.engine.document.getElementById(107360180).getProperty('value.value'); 
-var roundedValue1 = Math.round(result1 * 1) / 1;
-loader.engine.document.getElementById(107363830).setValue(({"value": roundedValue1})); 
+var result2 = loader.engine.document.getElementById(107360180).getProperty('value.value'); 
+var roundedValue1 = Math.round(result2 * 1) / 1;
+loader.engine.document.getElementById(107363830).setValue(({"value": roundedValue2})); 
 };
 
 window.onclick = roundResult;
