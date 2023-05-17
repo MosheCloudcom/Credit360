88d4b55048c73d0a7bdb9815717ee090adcf0930
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2023-05-17 14:49:22 +0300

    Add files via upload

---
+++ roundageclickobject.js
@@ -0,0 +1,8 @@
+function roundResult() {
+var result1 = loader.engine.document.getElementById(107353245).getProperty('value.value'); 
+var objtoclick = loader.engine.document.getElementById(107356600);
+var roundedValue1 = Math.round(result1 * 1) / 1;
+loader.engine.document.getElementById(107353245).setValue(({"value": roundedValue1})); 
+};
+objtoclick.click()
+//window.onchange = roundResult;
