8561261195cb851776be759d685f388482754912
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2023-05-21 17:37:43 +0300

    Update updateotherfield.js

--- updateotherfield.js
+++ updateotherfield.js
@@ -8,7 +8,7 @@ var roundedValue2 = Math.round(result2 * 1) / 1;
 loader.engine.document.getElementById(107363830).setValue(({"value": roundedValue2})); 
 
 var result3 = loader.engine.document.getElementById(107375878).getProperty('value.value'); 
-loader.engine.document.getElementById(107363830).setValue(({"value": result3})); 
+loader.engine.document.getElementById(107427653).setValue(({"value": result3})); 
 
 
 };
