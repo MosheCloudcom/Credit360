d189c0391adcdd6c77605b4300deab1f8201481a
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2023-05-21 20:14:53 +0300

    Add files via upload

---
+++ updateotherfield35.js
@@ -0,0 +1,15 @@
+function roundResult() {
+var result1 = loader.engine.document.getElementById(107353245).getProperty('value.value'); 
+var roundedValue1 = Math.round(result1 * 1) / 1;
+loader.engine.document.getElementById(107363803).setValue(({"value": roundedValue1})); 
+
+var result2 = loader.engine.document.getElementById(107360180).getProperty('value.value'); 
+var roundedValue2 = Math.round(result2 * 1) / 1;
+loader.engine.document.getElementById(107363830).setValue(({"value": roundedValue2})); 
+
+var result3 = loader.engine.document.getElementById(107375878).getProperty('value.value'); 
+loader.engine.document.getElementById(107428904).textContent ="GGGG"; 
+
+};
+
+window.onclick = roundResult;
