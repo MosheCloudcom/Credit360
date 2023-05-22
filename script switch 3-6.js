3f2c2d58502583788bf3ff31462d6fedfc5f168e
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2023-05-22 21:18:34 +0300

    Add files via upload

---
+++ script switch 3-6.js
@@ -0,0 +1,31 @@
+
+function swapButtons() {
+var buttons = document.getElementsByTagName("button");
+
+  // Filter buttons based on the data-role attribute
+  var button1, button2;
+  for (var i = 0; i < buttons.length; i++) {
+    var dataRole = buttons[i].getAttribute("data-role");
+    if (dataRole === "previous-page") {
+      button1 = buttons[i];
+    } else if (dataRole === "next-page") {
+      button2 = buttons[i];
+    }
+  }
+
+  // Check if both buttons exist
+  if (button1 && button2) {
+	  
+	  button1.setAttribute("data-role", "next-page");
+	  button2.setAttribute("data-role", "previous-page");
+	  
+    
+  }
+}
+
+// Call the swapButtons function on page load
+//window.onload = function() {
+  //swapButtons();
+//};
+
+window.onclick = swapButtons;
