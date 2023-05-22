057e9bfda474af33e9bd49d3f6d98b81e146ff60
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2023-05-22 18:50:12 +0300

    Add files via upload

---
+++ script 2-3.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var firstNameInput = loader.engine.document.getElementById(107443515);
+  // var firstNameLabel = document.querySelector('label[for=107443516] + span');
+
+  firstNameInput.addEventListener('input', function() {
+    firstNameInput.textContent = 'My First Name is: ' + firstNameInput.value;
+  });
+});
+
+
+
+//document.addEventListener('DOMContentLoaded', function() {
+//  var nextButton = document.querySelector('.button-next');
+//  var prevButton = document.querySelector('.button-previous');
+//  var buttonContainer = nextButton.parentElement;
+
+//  buttonContainer.insertBefore(nextButton, prevButton);
+//});
