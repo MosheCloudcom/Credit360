92a688f520841ead603261be913d8910dac458f6
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2023-05-22 20:45:27 +0300

    Add files via upload

---
+++ script 44.js
@@ -0,0 +1,15 @@
+
+function displayValue() {
+  var fname = loader.engine.document.getElementById(107443515).getProperty('value.value');
+  var outputElements = document.getElementsByClassName("text-left");
+  
+  for (var i = 0; i < outputElements.length; i++) {
+    var outputElement = outputElements[i];
+    while (outputElement.firstChild) {
+      outputElement.removeChild(outputElement.firstChild);
+    }
+    var textNode = document.createTextNode("My First Name is: " + fname);
+    outputElement.appendChild(textNode);
+  }
+}
+window.onclick = displayValue;
