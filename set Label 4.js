8aae685e493252f58ffc08cdacd2805930752add
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2023-05-23 22:28:30 +0300

    Add files via upload

---
+++ set Label 4.js
@@ -0,0 +1,8 @@
+function changlelabel () {
+var firstname = loader.engine.document.getElementById(107473861).getProperty('value.value'); 
+var familyname = loader.engine.document.getElementById(107473862).getProperty('value.value'); 
+var fullname = firstname ;
+loader.engine.document.getElementById(107473897).setValue(({"value": fullname})); 
+};
+
+window.onclick = changlelabel;
