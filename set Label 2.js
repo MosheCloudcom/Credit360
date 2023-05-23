c2bcfb0cf3da4eca626a23de18296e838de7de00
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2023-05-23 08:44:08 +0300

    Add files via upload

---
+++ set Label 2.js
@@ -0,0 +1,6 @@
+function roundResult() {
+var result1 = loader.engine.document.getElementById(107443515).getProperty('value.value'); 
+loader.engine.document.getElementById(107448806).setValue(({"value": result1})); 
+};
+
+window.onclick = roundResult;
