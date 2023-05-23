df62b025dc0c090a09d013de1650d964f03b772e
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2023-05-23 22:16:23 +0300

    Add files via upload

---
+++ comma seperator 4.js
@@ -0,0 +1,9 @@
+function addSeparators() {
+	
+var number1 = loader.engine.document.getElementById(107473736).getProperty('value.value'); // - formula field or number field
+var formattednumber = number1.toLocaleString('en-US');
+
+loader.engine.document.getElementById(107448806).setValue(({"value": formattednumber})); //- short text field
+};
+
+window.onclick = addSeparators;
