4007fb522a229c3ad19a8f06bb9349368c1ab73c
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2023-05-23 22:35:44 +0300

    Add files via upload

---
+++ credit360-2.js
@@ -0,0 +1,24 @@
+function roundResult() {
+var result1 = loader.engine.document.getElementById(107353245).getProperty('value.value'); 
+var roundedValue1 = Math.round(result1 * 1) / 1;
+loader.engine.document.getElementById(107363803).setValue(({"value": roundedValue1})); 
+
+var result2 = loader.engine.document.getElementById(107360180).getProperty('value.value'); 
+var roundedValue2 = Math.round(result2 * 1) / 1;
+loader.engine.document.getElementById(107363830).setValue(({"value": roundedValue2})); 
+
+var result3 = loader.engine.document.getElementById(107375878).getProperty('value.value'); 
+
+
+};
+
+function fillfullname () {
+var firstname = loader.engine.document.getElementById(107375878).getProperty('value.value'); 
+var familyname = loader.engine.document.getElementById(107375862).getProperty('value.value'); 
+var fullname = firstname + " " + familyname;
+
+loader.engine.document.getElementById(107474091).setValue(({"value": fullname})); 
+};
+
+window.onclick = fillfullname;
+window.onclick = roundResult;
