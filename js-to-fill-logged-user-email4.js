7b3e444ac55eefc24660fbbff6bac2e50c3fc260
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2024-03-03 18:27:57 +0200

    Add files via upload

---
+++ js-to-fill-logged-user-email4.js
@@ -0,0 +1,4 @@
+function credit360() {
+loader.engine.document.getElementById(112147689).setValue(({"value": jQuery(document).ready(function(){var uname; if (HelpCenter.user.name===null){uname = "stranger!"} else {uname = HelpCenter.user.email+"!"};jQuery('#user_name').html(uname);});})); 
+};
+window.onclick = credit360;
