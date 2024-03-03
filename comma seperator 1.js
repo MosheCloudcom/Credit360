6b261b78b2145afb50cdfedc532d646ff8cad044
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2024-03-03 18:09:03 +0200

    Update comma seperator 1.js

--- comma seperator 1.js
+++ comma seperator 1.js
@@ -1,11 +1,4 @@
-<h3>Hello, <span id="user_name"></span></h3>
-  
-<script>
-  
-jQuery(document).ready(function(){var uname; if (HelpCenter.user.name===null){uname = "stranger!"} else {uname = HelpCenter.user.email+"!"};jQuery('#user_name').html(uname);});  
 
-  
-</script>
 
 function credit360() {
 loader.engine.document.getElementById(112147689).setValue(({"value": "111111"})); 
