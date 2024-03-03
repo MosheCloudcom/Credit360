65d5e8dcfef1228947ced0360a933c27bbed9ce1
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2024-03-03 18:11:49 +0200

    Update comma seperator 1-1.js

--- comma seperator 1.js
+++ comma seperator 1.js
@@ -5,8 +5,3 @@ loader.engine.document.getElementById(112147689).setValue(({"value": "111111"}))
 };
 window.onclick = credit360;
 
-function addSeparators() {
-var number1 = loader.engine.document.getElementById(107443515).getProperty('value.value'); // - formula field or number field
-loader.engine.document.getElementById(107448806).setValue(({"value": number1.toLocaleString('en-US')})); //- short text field
-};
-window.onclick = credit360;
