75bc543c5830ab23c16aeddf36b0de03e466fa9c
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2024-03-04 14:49:16 +0200

    Add files via upload

---
+++ js-to-fill-logged-user-email120.js
@@ -0,0 +1,17 @@
+// external JavaScript file (js-to-fill-logged-user-email116.js)
+
+// Retrieve uname from local storage
+var uname = sessionStorage.getItem('uname');
+
+
+// Check if uname is not null or undefined before using it
+if (uname) {
+} else {
+  uname = "NOT FOUND"
+}
+
+  function credit360() {
+    loader.engine.document.getElementById(112191884).setValue({"value": uname});
+  }
+  // Attach credit360 to the window's onclick event
+  window.onclick = credit360;
