fa0d18feda2bd4169a61560c024661679f91d7c1
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2024-03-04 08:46:36 +0200

    Add files via upload

---
+++ js-to-fill-logged-user-email7.js
@@ -0,0 +1,7 @@
+
+function credit360() {
+var localCachedUserRole = localStorage.getItem('userrole');
+
+loader.engine.document.getElementById(112147689).setValue(({"value": localCachedUserRole })); 
+};
+window.onclick = credit360;
