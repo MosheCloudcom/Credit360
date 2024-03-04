c074b8f3f21b8a54134d0f3d60c47f5cd184b8ed
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2024-03-04 14:23:50 +0200

    Add files via upload

---
+++ js-to-fill-logged-user-email117.js
@@ -0,0 +1,8 @@
+// Retrieve uname from local storage
+var uname = localStorage.getItem('uname');
+
+function credit360() {
+loader.engine.document.getElementById(112191884).setValue({"value": uname});
+
+};
+window.onclick = credit360;
